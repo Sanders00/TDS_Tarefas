acfe0ba434e37162283c12db3e60353e2b25b7e3
Sanders00 <96478702+Sanders00@users.noreply.github.com>
2024-03-17 19:26:44 -0300

    TPTE02

--- TPTE01/obj/Debug/net8.0/TPTE01.AssemblyInfo.cs
+++ TPTE01/obj/Debug/net8.0/TPTE01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPTE01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5a98f50e35192605d47fddca2f2b228b1b49d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d73600b3e5f4f88d9a536ef607b738601ec9edb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPTE01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPTE01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
